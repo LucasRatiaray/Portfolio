be6b82c96cb9a7f47206b3676a7f4d7af39e54e4
LucasRatiaray <contact.ratiaray@gmail.com>
2023-03-29 00:15:28 +0200

    fromulaire de login + template login

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -34,3 +34,17 @@ window.addEventListener("scroll", () => {
     }
   });
 });
+
+/*--------------------*/
+/* SHOW PASSWORD
+/*--------------------*/
+const passwordInput = document.getElementById('password');
+const showPasswordCheckbox = document.getElementById('show-password');
+
+showPasswordCheckbox.addEventListener('change', function() {
+  if (showPasswordCheckbox.checked) {
+    passwordInput.type = 'text';
+  } else {
+    passwordInput.type = 'password';
+  }
+});
